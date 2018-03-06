3d3862c473f15078c811e32d354d0cd897a091e5
Maxim <stemax@OldMacMiniTurbo.local.rg>
2018-03-06 11:25:14 +0200

    everything is fixed v4

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Levels : MonoBehaviour {
+
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+
+	public void QuitBtnClick() {
+		Application.Quit();
+	}
+
+	public void OnMouseDown () {
+		Debug.LogWarning ("Button Pressed@");
+		SceneManager.LoadScene (2);
+	}
+}
